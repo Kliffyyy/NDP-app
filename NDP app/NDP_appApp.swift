1d1b37dcef50f6921e4a0e8a12142ea116693ce1
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-07-22 10:12:53 +0800

    Initial Commit

---
+++ NDP app/NDP_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDP_appApp.swift
+//  NDP app
+//
+//  Created by klifton Cheng stu on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct NDP_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
